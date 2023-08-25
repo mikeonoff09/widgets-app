44605e029b566683de819ba11840cbfefe0e2333
Miguel Lara <miguel.larasj@gmail.com>
2023-08-25 17:19:08 -0500

    Home Screen implemented

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,38 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('HomeScreen'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListView.builder(
+      // physics: const BouncingScrollPhysics(),
+      itemCount: appMenuItems.length,
+      itemBuilder: (BuildContext context, int index) {
+        final menuItem = appMenuItems[index];
+        return ListTile(
+          title: Text(menuItem.title),
+          subtitle: Text(menuItem.subtitle),
+          leading: Icon(
+            menuItem.icon,
+            color: colors.primary,
+          ),
+          trailing: Icon(
+            Icons.arrow_forward_ios_rounded,
+            color: colors.primary,
+          ),
+          onTap: () {
+            // TODO: add navigation
+          },
+        );
+      },
     );
   }
 }
